169aed8fbf499aa586be40605c116a1e4de9daba
aeolteanu <aeolteanu@gmail.com>
2021-01-23 14:25:52 +0200

    Task 1: Static vs Non-static,Final

--- src/main/java/fundamentals/Main.java
+++ src/main/java/fundamentals/Main.java
@@ -5,15 +5,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        //Write an application that will read diameter of a circle (variable of type float) and
-        //calculate perimeter of given circle.
-        //Firstly, assume π = 3.14. Later, use value of π from built-in Math class
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Insert diameter: ");
-        float diameter = scanner.nextFloat();
-        float pi = (float) Math.PI;
-        System.out.println("The perimeter has the following size: " + (diameter * pi));
+//        System.out.println("Hello world!");
+//        TasksExercises tasksExercises = new TasksExercises();
+//        tasksExercises.perimeter();
+//        TasksExercises.perimeter();
+        TasksExercises tasksExercises1= new TasksExercises();
+        TasksExercises tasksExercises2= new TasksExercises();
+        System.out.println("x= "+ tasksExercises1.x);
+        System.out.println("y= "+ tasksExercises1.y);
+        System.out.println("y= "+ tasksExercises2.x);
+        tasksExercises1.x=22;
+        tasksExercises2.y=3;
+        System.out.println("x1= "+ tasksExercises1.x);
+        System.out.println("x2= "+ tasksExercises2.x);
+        System.out.println("y1= "+ tasksExercises1.y);
+        System.out.println("y2= "+ tasksExercises2.y);
+
+        //Task1
+        tasksExercises1.perimeter();
+
 
     }
 }
