8d71121b894227ac99fa7561b8fb5f1f4a6cdc67
aeolteanu <aeolteanu@gmail.com>
2021-01-27 14:50:24 +0200

    Homework #1

--- src/main/java/fundamentals/Main.java
+++ src/main/java/fundamentals/Main.java
@@ -10,19 +10,24 @@ public class Main {
 //        tasksExercises.perimeter();
 //        TasksExercises.perimeter();
         TasksExercises tasksExercises1= new TasksExercises();
-        TasksExercises tasksExercises2= new TasksExercises();
-        System.out.println("x= "+ tasksExercises1.x);
-        System.out.println("y= "+ tasksExercises1.y);
-        System.out.println("y= "+ tasksExercises2.x);
-        tasksExercises1.x=22;
-        tasksExercises2.y=3;
-        System.out.println("x1= "+ tasksExercises1.x);
-        System.out.println("x2= "+ tasksExercises2.x);
-        System.out.println("y1= "+ tasksExercises1.y);
-        System.out.println("y2= "+ tasksExercises2.y);
+//        TasksExercises tasksExercises2= new TasksExercises();
+//        System.out.println("x= "+ tasksExercises1.x);
+//        System.out.println("y= "+ tasksExercises1.y);
+//        System.out.println("y= "+ tasksExercises2.x);
+//        tasksExercises1.x=22;
+//        tasksExercises2.y=3;
+//        System.out.println("x1= "+ tasksExercises1.x);
+//        System.out.println("x2= "+ tasksExercises2.x);
+//        System.out.println("y1= "+ tasksExercises1.y);
+//        System.out.println("y2= "+ tasksExercises2.y);
 
         //Task1
-        tasksExercises1.perimeter();
+//        tasksExercises1.perimeter();
+
+//        tasksExercises1.ex1();
+//        tasksExercises1.ex2();
+        tasksExercises1.ex3();
+        tasksExercises1.tooMuchTooLittle();
 
 
     }
--- src/main/java/fundamentals/TasksExercises.java
+++ src/main/java/fundamentals/TasksExercises.java
@@ -1,11 +1,12 @@
 package fundamentals;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class TasksExercises {
     static float pi = (float) Math.PI;
     int x = 10;
-    static int y=1;
+    static int y = 1;
 
 
     public static void perimeter() {
@@ -21,4 +22,85 @@ public class TasksExercises {
         System.out.println("The perimeter has the following size: " + diameter * pi);
     }
 
+    public static void ex1() {
+        // Write an application that takes a positive number from the user (type int) and prints all prime numbers greater than 1 and less than the given number.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input the number");
+        int a = scanner.nextInt();
+        for (int x = 1; x < a; x++) {
+            System.out.println("Prime numbers smaller than a: " + x);
+        }
+
+
+    }
+
+    public static void ex2() {
+        //Write an application calculating BMI (Body Mass Index) and checking if itЀs optimal or not.
+        //Your application should read two variables: weight (in kilograms, type float) and height
+        //(in centimeters, type int). BMI should be calculated given following formula:
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert your height: ");
+        int height = scanner.nextInt();
+        System.out.println("Insert your weight: ");
+        float weight = scanner.nextFloat();
+        float bmi = weight / height;
+        System.out.println("Your BMI is " + bmi);
+        if (bmi > 18.5 && bmi < 24.9) {
+            System.out.println("Your BMI is optimal");
+        } else System.out.println("Your BMI is not optimal");
+
+
+    }
+
+    public static void ex3() {
+        //Write an application that takes a positive number from the user (type int) and writes all
+        //numbers from 1 to the given number, each on the next line, with the following changes:
+        //● in place of numbers divisible by 3, instead of a number the program should print "Fizz"
+        //● in place of numbers divisible by 7, instead of a number the program should write
+        //"Buzz"
+        //● if the number is divisible by both 3 and 7, the program should print "Fizz buzz"
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduceti numarul: ");
+
+        int a = scanner.nextInt();
+        for (int x = 1; x < a; x++) {
+            System.out.println(x);
+
+        }
+
+
+        if ((a % 3 == 0) && (a % 7 == 0)) {
+            System.out.println("FizzBuz");
+        } else if (a % 3 == 0) {
+            System.out.println("fizz");
+        } else if (a % 7 == 0) {
+            System.out.println("buzz");
+        } else {
+            System.out.println(" Nici un numar nu este divizibil cu 3 sau 7");
+
+        }
+
+
+    }
+
+    public static void tooMuchTooLittle() {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int a = random.nextInt(10);
+        System.out.println("Your number is " + a);
+        int b = scanner.nextInt();
+        do {
+            if (b < a) {
+                System.out.println("Not enough!");
+            } else if (b > a) {
+                System.out.println("Too much!");
+            } else System.out.println("Congrulations");
+        } while (b == a);
+
+    }
 }
+
+
+
+
